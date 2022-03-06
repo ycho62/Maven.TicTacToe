3dcbc1ed5a605fcd943f0d15690da00527f45461
Yun <yun@zipcoders-MacBook-Pro-2.local>
2022-03-05 20:14:45 -0500

    final

--- src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
+++ src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
@@ -4,23 +4,80 @@ package rocks.zipcodewilmington.tictactoe;
  * @author leon on 6/22/18.
  */
 public class Board {
+    Character[][] matrix;
+
     public Board(Character[][] matrix) {
+        this.matrix = matrix;
     }
 
     public Boolean isInFavorOfX() {
-        return null;
+
+        for (int i = 0; i <= 9; i++) {
+            if (matrix[0][0] == 'X' && matrix[0][1] == 'X' && matrix[0][2] == 'X') {
+                return true;
+            } else if (matrix[1][0] == 'X' && matrix[1][1] == 'X' && matrix[1][2] == 'X') {
+                return true;
+            } else if (matrix[2][0] == 'X' && matrix[2][1] == 'X' && matrix[2][2] == 'X') {
+                return true;
+            } else if (matrix[0][0] == 'X' && matrix[1][0] == 'X' && matrix[2][0] == 'X') {
+                return true;
+            } else if (matrix[0][1] == 'X' && matrix[1][1] == 'X' && matrix[2][1] == 'X') {
+                return true;
+            } else if (matrix[0][2] == 'X' && matrix[1][2] == 'X' && matrix[2][2] == 'X') {
+                return true;
+            } else if (matrix[0][0] == 'X' && matrix[1][1] == 'X' && matrix[2][2] == 'X') {
+                return true;
+            } else if (matrix[0][2] == 'X' && matrix[1][1] == 'X' && matrix[2][0] == 'X') {
+                return true;
+            }
+        }
+        return false;
     }
 
     public Boolean isInFavorOfO() {
-        return null;
+
+        for (int i = 0; i <= 9; i++) {
+            if (matrix[0][0] == 'O' && matrix[0][1] == 'O' && matrix[0][2] == 'O') {
+                return true;
+            } else if (matrix[1][0] == 'O' && matrix[1][1] == 'O' && matrix[1][2] == 'O') {
+                return true;
+            } else if (matrix[2][0] == 'O' && matrix[2][1] == 'O' && matrix[2][2] == 'O') {
+                return true;
+            } else if (matrix[0][0] == 'O' && matrix[1][0] == 'O' && matrix[2][0] == 'O') {
+                return true;
+            } else if (matrix[0][1] == 'O' && matrix[1][1] == 'O' && matrix[2][1] == 'O') {
+                return true;
+            } else if (matrix[0][2] == 'O' && matrix[1][2] == 'O' && matrix[2][2] == 'O') {
+                return true;
+            } else if (matrix[0][0] == 'O' && matrix[1][1] == 'O' && matrix[2][2] == 'O') {
+                return true;
+            } else if (matrix[0][2] == 'O' && matrix[1][1] == 'O' && matrix[2][0] == 'O') {
+                return true;
+            }
+        }
+        return false;
     }
 
     public Boolean isTie() {
-        return null;
+        if (isInFavorOfX() || isInFavorOfO()) {
+
+            return false;
+        } else {
+            return true;
+        }
     }
 
     public String getWinner() {
-        return null;
-    }
 
+        if (isInFavorOfX()) {
+            return "X";
+
+        } else if (isInFavorOfO()) {
+            return "O";
+
+        } else
+            return "";
+
+    }
 }
+
